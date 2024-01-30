7be59fb3aaff056b3a54a4484997b1369c596e2c
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-01-30 22:57:48 +0100

    alpha translation

---
+++ R/i18n/translatify.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# ./translatify.sh transation_file
+
+for file in ../*; do
+	SOURCE=$file;
+	base=$(echo "${SOURCE%.*}" | sed "s/.*\///");
+	DEST="../$(echo ${base} | sed "s/_i18n//").R";
+	if [[ -f $DEST ]]; then
+		while read -r line; do
+			var=$(echo $line | sed "s/\ *<-·*$//");
+			string=$(echo $line | sed "s/^.*<-\ *//");
+			sed -i "s/$string/$var/g" "$DEST"
+		done < ${file}
+		echo "$DEST" found
+	else
+		echo "$DEST" not found. Continuing.
+	fi;
+done
