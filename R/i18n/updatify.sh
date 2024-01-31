5e2234a77a210ffd6a496c77450cdd261d97a2e4
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-01-31 09:20:57 +0100

    alpha translation

---
+++ R/i18n/updatify.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# ./updatify.sh file_to_translate
+
+DEST="$1"
+for file in *; do
+	SOURCE_NAME=$(echo "${file%.*}" | sed "s/.*\///");
+	DEST_NAME=$(echo "${DEST%.*}" | sed "s/.*\///");
+	if [[ ${SOURCE_NAME} == ${DEST_NAME} ]]; then
+		echo "same"
+#		exit
+	fi
+	echo $SOURCE_NAME
+	while read -r line_src; do
+		var_src=$(echo $line_src | sed "s/\ *<-\s*.*//");
+		string_src=$(echo $line_src | sed "s/^.*<-\ *//");
+		while read -r line_dest; do
+			if [[ ${string_src} == ${line_dest} ]]; then
+				#exit
+				sed -i "s/${line_dest}/${line}/g" ${DEST}
+				continue
+			fi
+			var_dest=$(echo $line_dest | sed "s/\ *<-\s*.*//");
+			string_dest=$(echo $line_dest | sed "s/^.*<-\ *//");
+			#sed -i "s/${string}/${var}/g" "$DEST"
+			if [[ ${string_src} == ${string_dest} ]]; then
+				#exit
+				sed -i "s/${line_dest}/${line}/g" ${DEST}
+				continue
+			fi
+		done < ${DEST}
+	done < ${file}
+done
