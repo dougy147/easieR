60ff781846c84c0894430281a19fc789c36ccea1
dougy147 <16362303+dougy147@users.noreply.github.com>
2024-01-31 15:11:56 +0100

    Update translation scripts

--- R/i18n/02_translatify.sh
+++ R/i18n/02_translatify.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
-# ./translatify.sh transation_file
+# ./translatify.sh [translation_file_i18n]
 
-for file in *; do
+# - 1: Grab a translation_file (i18n)
+# - 2: Find the coding file it corresponds to
+# - 3: Replace in the coding file every occurrence of strings by variables' names
+
+if [[ -z $1 ]]; then
+	FILE='*'
+else
+	FILE=$1
+fi
+
+read -p "Start modifying files in './R' ? (y/N) " ans
+if [[ ! $(echo $ans | grep -io "y") ]]; then
+	echo "Canceled."
+	exit
+fi
+
+for file in $FILE; do
 	SOURCE=$file;
 	base=$(echo "${SOURCE%.*}" | sed "s/.*\///");
 	DEST="../$(echo ${base} | sed "s/_i18n//").R";
@@ -14,7 +30,7 @@ for file in *; do
 			string=$(echo $line | sed "s/^.*<-\ *//" | sed "s/\&/\\\&/g");
 			sed -i "s/${string}/${var}/g" "$DEST"
 		done < ${file}
-		echo "$DEST" found
+		echo "$DEST" done.
 	else
 		echo "$DEST" not found. Continuing.
 	fi;
--- R/i18n/03_updatify.sh
+++ R/i18n/03_updatify.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# Useful for translation of new coding files
+
 # ./updatify.sh [source_file] file_to_translate
+# - 1 : Grab a single translation file (i18n)
+# - 2 : Check if the strings it contains are already translated in other translation files
+# - 3 : if yes, store the variable's name directly in the current translation file
+# - ... : do the same for the rest
 
 if [[ -z $2 ]]; then
 	SOURCE="*"
@@ -9,29 +15,33 @@ else
 	SOURCE="$1"
 	DEST="$2"
 fi
+
+if [[ -z $1 ]]; then
+	echo "Usage: ./updatify.sh [source_file] file_to_translate"
+	exit
+fi
+
 #DEST="$1"
 for file in $SOURCE; do
 	SOURCE_NAME=$(echo "${file%.*}" | sed "s/.*\///");
 	DEST_NAME=$(echo "${DEST%.*}" | sed "s/.*\///");
 	if [[ ${SOURCE_NAME} == ${DEST_NAME} ]]; then
-		echo "same"
-#		exit
+		continue
 	fi
-	echo $SOURCE_NAME
+	echo "Comparing '$DEST_NAME' with '$SOURCE_NAME'"
 	while read -r line_src; do
 		var_src=$(echo $line_src | sed "s/\ *<-\s*.*//");
 		string_src=$(echo $line_src | sed "s/^.*<-\ *//");
 		while read -r line_dest; do
 			if [[ ${string_src} == ${line_dest} ]]; then
-				#exit
+				echo " > (0) Found identical strings, giving same variable name."
 				sed -i "s/${line_dest}/${line_src}/g" ${DEST}
 				continue
 			fi
 			var_dest=$(echo $line_dest | sed "s/\ *<-\s*.*//");
 			string_dest=$(echo $line_dest | sed "s/^.*<-\ *//");
-			#sed -i "s/${string}/${var}/g" "$DEST"
 			if [[ ${string_src} == ${string_dest} ]]; then
-				#exit
+				echo " > (1) Found identical strings, giving same variable name."
 				sed -i "s/${line_dest}/${line_src}/g" ${DEST}
 				continue
 			fi
